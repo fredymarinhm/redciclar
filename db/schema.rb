e49eb4df9ed9413aad40c234f3eff561297ec57a
Fredy Marín <fredymarinduarte@hotmail.com>
2018-07-25 12:10:26 -0500

    Creacion de modelos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180717153558) do
+ActiveRecord::Schema.define(version: 20180725170526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20180717153558) do
     t.string   "picture"
     t.integer  "user_id"
     t.index ["user_id"], name: "index_images_on_user_id", using: :btree
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
