259a97229840b609953736b9ea2af3a49a927e1b
Fredy <fredymarinduarte@hotmail.com>
2018-08-02 15:11:13 -0500

    Actualización del script

--- db/Script.sql
+++ db/Script.sql
@@ -9,30 +9,25 @@ INSERT INTO public.users(
 	VALUES ('prueba@prueba.com', '$2a$11$6/K8/R.dIC8BzAJ81tBlQOPKlQdUAVIiAR49qkKfBZZA0.Yz1NRWe', null, null, null, '2018-07-28', '2018-07-28', 'Prue', 'ba', 'C.C. 1234', '2018-07-28', null, null, null, null);
 
 --Insert points
-INSERT INTO public.points(id, date, amount, created_at, updated_at, user_id)
-	VALUES (1, '2018-07-28', 10, '2018-07-28', '2018-07-28', 1);
+INSERT INTO public.points(date, amount, created_at, updated_at, user_id)
+	VALUES ('2018-07-28', 10, '2018-07-28', '2018-07-28', 1);
 
 --Insert products
-INSERT INTO public.products(
-	id, name, description, price, amount, image, created_at, updated_at)
-	VALUES (1, 'puntos', 'producto puntos', 10, 10, null, '2018/07/28', '2018/07/28');
+INSERT INTO public.products(name, description, price, amount, image, created_at, updated_at)
+	VALUES ('puntos', 'producto puntos', 10, 10, null, '2018/07/28', '2018/07/28');
 
 --Insert recycling station
-INSERT INTO public.recycling_stations(
-	id, name, address, telephone, latitude, longitude, description, created_at, updated_at)
-	VALUES (1, 'Estación 1', 'Calle 100', '1234', 123.45, 123.45, 'Estación uno', '2018/07/28', '2018/07/28');
+INSERT INTO public.recycling_stations(name, address, telephone, latitude, longitude, description, created_at, updated_at)
+	VALUES ('Estación 1', 'Calle 100', '1234', 123.45, 123.45, 'Estación uno', '2018/07/28', '2018/07/28');
 
 --Insert store
-INSERT INTO public.stores(
-	id, name, telephone, email, address, created_at, updated_at)
-	VALUES (1, 'Tienda virutal', 'Tel 1234', 'tienda@virtual.com', 'Calle 100', '2018/07/28', '2018/07/28');
+INSERT INTO public.stores(name, telephone, email, address, created_at, updated_at)
+	VALUES ('Tienda virutal', 'Tel 1234', 'tienda@virtual.com', 'Calle 100', '2018/07/28', '2018/07/28');
 
 --Insert order
-INSERT INTO public.orders(
-	id, status, code, created_at, updated_at, user_id, recycling_station_id, store_id)
-	VALUES (1, 'Activa', 1234, '2018/07/28', '2018/07/28', 1, 1, 1);
+INSERT INTO public.orders(status, code, created_at, updated_at, user_id, recycling_station_id, store_id)
+	VALUES ('Activa', 1234, '2018/07/28', '2018/07/28', 1, 1, 1);
 	
 --Insert item
-INSERT INTO public.items(
-	id, description, price, amount, created_at, updated_at, product_id, order_id, points)
-	VALUES (1, 'Descripción prueba', 1000, 1000, '2018/07/28', '2018/07/28', 1, 1, 10);
+INSERT INTO public.items(description, price, amount, created_at, updated_at, product_id, order_id, points)
+	VALUES ('Descripción prueba', 1000, 1000, '2018/07/28', '2018/07/28', 1, 1, 10);
